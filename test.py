dbdb1683abeeced90aea1ce18c073b8e87b52c32
CookIsGood <Morkovo4ka513@yandex.ru>
2022-03-29 10:13:17 +0300

    Добавлена функции для распознавания

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 from services.DocumentService import DocumentService
+import base64
 
 service = DocumentService()
 
 name_doc = "Вопросы по Hive"
 #service._create_binary_pdf(name_doc)
 hello = service.pdf_to_binary(f"images/{name_doc}.pdf")
+print(hello)
 service.binary_to_pdf(bytes(hello), "new_pdf")
-service.find_coordinates("images", hello)
+
 
